f9980739b5558e3a5c9fd6d04588d79b88819682
Scott Taylor <scott@railsnewbie.com>
2008-12-22 13:59:58 -0500

    * Add spec_helper and spec.opts

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+module SpecHelperFunctions
+  def setup_tests
+    setup_database_connection
+  end
+  
+private
+  
+  def setup_database_connection
+    require 'rubygems'
+    require 'sqlite3'
+    require 'active_record'
+    require 'active_support'
+    
+    ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
+    ActiveRecord::Migration.verbose = false
+
+    ActiveRecord::Schema.define do  
+    end
+  end
+end
