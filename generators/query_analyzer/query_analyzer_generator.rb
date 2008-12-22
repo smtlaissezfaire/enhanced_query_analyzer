25a14777efdb96087cd33ca29141f6a2b2337059
Scott Taylor <scott@railsnewbie.com>
2008-12-22 16:00:54 -0500

    * Remove unused values from generators

--- generators/query_analyzer/query_analyzer_generator.rb
+++ generators/query_analyzer/query_analyzer_generator.rb
@@ -8,25 +8,4 @@ class QueryAnalyzerGenerator < Rails::Generator::Base
       }
     end
   end
-
-  def class_name
-    "QueryLog"
-  end
-
-  def custom_file_name
-    custom_name = class_name.underscore.downcase
-    custom_name = custom_name.pluralize if ActiveRecord::Base.pluralize_table_names
-  end
-
-  def yaffle_local_assigns
-    returning(assigns = {}) do
-      assigns[:migration_action] = "create"
-      assigns[:table_name] = custom_file_name
-      assigns[:attributes] = [Rails::Generator::GeneratedAttribute.new(:query, :string)]
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new(:query_time, :float)
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new(:explain, :string)
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new(:created_at, :datetime)
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new(:updated_at, :datetime)
-    end
-  end
 end
