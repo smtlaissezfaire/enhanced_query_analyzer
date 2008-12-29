7ab26ab47c8e76f73aa82baae82b70c1c8594f48
Scott Taylor <scott@railsnewbie.com>
2008-12-29 18:22:08 -0500

    * Move SelectRunner into it's own file

--- lib/enhanced_query_analyzer.rb
+++ lib/enhanced_query_analyzer.rb
@@ -1,76 +1,10 @@
 require File.dirname(__FILE__) + "/enhanced_query_analyzer/select_proxy"
 require File.dirname(__FILE__) + "/enhanced_query_analyzer/logging"
+require File.dirname(__FILE__) + "/enhanced_query_analyzer/select_runner"
 
 module EnhancedQueryAnalyzer
   extend Logging
   extend SelectProxy
-
-  class SelectRunner
-    def initialize(adapter, logging_on, explain_logging_on)
-      @adapter = adapter
-      @logging_on = logging_on
-      @explain_logging_on = explain_logging_on
-    end
-
-    attr_reader :logging_on
-
-    def logging_on?
-      @logging_on ? true : false
-    end
-
-    def explain_logging_on?
-      @explain_logging_on ? true : false
-    end
-
-    def select(sql, name)
-      if log_query?(sql)
-        benchmark_and_run_query(sql, name)
-      else
-        run_query(sql, name)
-      end
-    end
-
-    def run_query(sql, name = nil)
-      @adapter.old_select_aliased_by_query_analyzer(sql, name)
-    end
-
-    def benchmark_and_run_query(sql, name)
-      result = nil
-      time = Benchmark.realtime do
-        result = run_query(sql, name)
-      end
-
-      save_logged_query_time(sql, time)
-
-      result
-    end
-
-  private
-
-    def save_logged_query_time(sql, time)
-      if select_query?(sql)
-        begin
-          QueryLog.create(:query => sql, :explain => explain(sql), :query_time => time)
-        rescue Mysql::Error; end
-      end
-    end
-
-    def explain(sql)
-      explain_logging_on? ? run_query("explain #{sql}") : nil
-    end
-
-    def log_query?(sql)
-      logging_on? && table_for_selection?(sql)
-    end
-
-    def table_for_selection?(sql)
-      !(sql =~ /.*(explain|query_logs).*/i)
-    end
-
-    def select_query?(sql)
-      sql =~ /select/i
-    end
-  end
 end
 
 ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
