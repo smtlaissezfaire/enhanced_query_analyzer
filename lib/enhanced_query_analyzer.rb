59caceadbb36333c4af5ce7e426c916ce2082801
Scott Taylor <scott@railsnewbie.com>
2008-12-29 17:55:19 -0500

    * Refactor into EnhancedQueryAnalyzer

--- lib/enhanced_query_analyzer.rb
+++ lib/enhanced_query_analyzer.rb
@@ -10,28 +10,33 @@ module EnhancedQueryAnalyzer
     def reset_logging!
       @logging = nil
     end
+
+    def select(adapter, sql, name)
+      if EnhancedQueryAnalyzer.logging && !(sql =~ /.*(explain|query_logs).*/i)
+        result = nil
+        time = Benchmark.realtime do
+          result = adapter.old_select_aliased_by_query_analyzer(sql, name)
+        end
+
+        if sql =~ /select/i
+          begin
+            QueryLog.create(:query => sql, :explain => adapter.old_select_aliased_by_query_analyzer("explain #{sql}"), :query_time => time)
+          rescue Mysql::Error; end
+        end
+        
+        result
+      else
+        adapter.old_select_aliased_by_query_analyzer(sql, name)
+      end
+    end
   end
 end
 
 ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
+  public :select
   alias_method :old_select_aliased_by_query_analyzer, :select
 
   def select(sql, name = nil)
-    if EnhancedQueryAnalyzer.logging && !(sql =~ /.*(explain|query_logs).*/i)
-      result = nil
-      time = Benchmark.realtime do
-        result = old_select_aliased_by_query_analyzer(sql, name)
-      end
-
-      if sql =~ /select/i
-        begin
-          QueryLog.create(:query => sql, :explain => old_select_aliased_by_query_analyzer("explain #{sql}"), :query_time => time)
-        rescue Mysql::Error; end
-      end
-      
-      result
-    else
-      old_select_aliased_by_query_analyzer(sql, name)
-    end
+    EnhancedQueryAnalyzer.select(self, sql, name)
   end
 end
