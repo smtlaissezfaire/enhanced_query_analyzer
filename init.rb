8e214da4da11c32059b17f086d8ccc053d7ff58e
Scott Taylor <scott@railsnewbie.com>
2009-01-05 19:27:25 -0500

    Dont' redefine select on the mysql adapter if it has already been redefined

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require File.dirname(__FILE__) + "/lib/enhanced_query_analyzer"
 
 ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
-  public :select
-  alias_method :old_select_aliased_by_query_analyzer, :select
+  unless defined?(:old_select_aliased_by_query_analyzer)
+    public :select
+    alias_method :old_select_aliased_by_query_analyzer, :select
 
-  def select(sql, name = nil)
-    EnhancedQueryAnalyzer.select(self, sql, name)
+    def select(sql, name = nil)
+      EnhancedQueryAnalyzer.select(self, sql, name)
+    end
   end
 end
