e759d348997a1cda3decf01685f9daf3f7a732ca
Scott Taylor <scott@railsnewbie.com>
2009-12-28 19:17:06 -0500

    Whitespace

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + "/lib/enhanced_query_analyzer"
 
 ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
   if !instance_methods.include?("old_select_aliased_by_query_analyzer")
-    public :select
+    public       :select
     alias_method :old_select_aliased_by_query_analyzer, :select
 
     def select(sql, name = nil)
